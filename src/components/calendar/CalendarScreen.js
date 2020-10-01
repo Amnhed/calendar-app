27b240741af826d7279aea7e4ff75379f25c34ea
Amnhed <aalhe0.0@gmail.com>
2020-10-01 13:27:16 -0500

    Configuracion de calendario
    Helper de configuracion de nombre de botones, Cambiar idioma y mostrar un evento en el calendar screen

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -3,18 +3,39 @@ import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 
 import { Navbar } from '../ui/Navbar'
+import { messages } from '../../helpers/calendar-messages-es'
+
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'moment/locale/es'
+
+moment.locale('es')
 
 const localizer = momentLocalizer(moment)
+
 
 const myEventsList = [{
     title: 'Cumpleaños del jefe',
     start: moment().toDate(),
     end: moment().add( 2, 'hours' ).toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    messages:messages
 }]
 
 export const CalendarScreen = () => {
+    //Personalizamos los estilos
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        const style = {
+            backgroundcolor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return{
+            style
+        }
+    }
+
     return (
         <div className='calendar-screen'>
             <Navbar />
@@ -24,6 +45,9 @@ export const CalendarScreen = () => {
                 startAccessor="start"
                 endAccessor="end"
                 style={{ height: 500 }}
+                messages= { messages }
+                eventPropGetter={ eventStyleGetter }
+
             />
         </div>
     )
