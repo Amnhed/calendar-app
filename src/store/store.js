f66053ee8f9e8cfb4eab0c5467701cb84cae86ba
Amnhed <aalhe0.0@gmail.com>
2020-10-16 15:38:08 -0500

    Configuracion del store
    configuración de store
    Obtengo composeEnhancers de la documentación de redux devotools
    Incluyo el root reducer
    Configuro el aaplymidlleware thunk para peticiones asíncronas

---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "../reducers/rootReducer";
+
+// Obtengo composeEnhancers de las intrucciones de redux  redux-devtools-extension
+//https://github.com/zalmoxisus/redux-devtools-extension#usage
+//importo el compose
+//Si ecisten las herrmientas las configurara de otra forma lo deja pasar
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+//APlico el middleware thunk
+export const store = createStore(
+    rootReducer,
+    composeEnhancers( 
+        applyMiddleware(thunk) 
+    )
+) 
